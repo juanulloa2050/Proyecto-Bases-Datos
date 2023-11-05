535ea3f53e71414b785a38ffd5ca70272309667d
SantiagoSantafe <andressasi@unisabana.edu.co>
2023-11-04 23:02:50 -0500

    Refactor code for better organization and user experience

--- src/proyecto_bases_datos/InterfazController.java
+++ src/proyecto_bases_datos/InterfazController.java
@@ -1,28 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
 package proyecto_bases_datos;
 
+import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
 import java.util.ResourceBundle;
-
-import javax.swing.JOptionPane;
-
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
-/**
- * FXML Controller class
- *
- * @author juanu
- */
 public class InterfazController implements Initializable {
-    JDBC connection = new JDBC();
+    JDBC connection = JDBC.getInstance();
+    private Connection conn;
+
     @FXML
     private AnchorPane T;
     @FXML
@@ -36,26 +33,29 @@ public class InterfazController implements Initializable {
     @FXML
     private Button btn_acceder;
 
-    /**
-     * Initializes the controller class.
-     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
-    public void acceder_action() {
+
+    public void acceder_action() throws IOException {
         String usuario = txt_usr.getText();
         String puerto = txt_puerto.getText();
-        String maquina = txt_maquina.getText();
         String clave = txt_clave.getText();
-        if (usuario.isEmpty() || puerto.isEmpty() || maquina.isEmpty() || clave.isEmpty()) {
-            // Mostrar un mensaje de error
-            JOptionPane.showMessageDialog(null,"Por favor, rellene todos los campos.");
+
+        if (usuario.isEmpty() || puerto.isEmpty() || clave.isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error Dialog");
+            alert.setHeaderText(null);
+            alert.setContentText("Por favor, rellene todos los campos.");
+            alert.showAndWait();
         } else {
-            // Aquí puedes continuar con tu lógica de negocio
             String url = "jdbc:mysql://localhost:"+puerto;
-            connection.conectarBase(url, usuario, clave);
+            conn=connection.conectarBase(url, usuario, clave);
+
+            Parent root = FXMLLoader.load(getClass().getResource("Menu.fxml"));
+            Stage stage = (Stage) btn_acceder.getScene().getWindow();
+            stage.setScene(new Scene(root));
         }
-        JOptionPane.showMessageDialog(null, "Hola");
     }
 }
