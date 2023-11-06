6f8e8a35fcaf3b9582301aed06aac456ab839f64
SantiagoSantafe <andressasi@unisabana.edu.co>
2023-11-06 12:30:50 -0500

    Add functionality to modify and delete table records

--- src/proyecto_bases_datos/TablasController.java
+++ src/proyecto_bases_datos/TablasController.java
@@ -4,14 +4,21 @@
  */
 package proyecto_bases_datos;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TabPane;
+import javafx.stage.Stage;
+import proyecto_bases_datos.managment.JDBC;
 
 /**
  * FXML Controller class
@@ -19,6 +26,8 @@ import javafx.scene.control.TabPane;
  * @author juanu
  */
 public class TablasController implements Initializable {
+    public static JDBC conection;
+    String dataBaseSelected;
 
     @FXML
     private TabPane TabPane_Tablas;
@@ -48,9 +57,24 @@ public class TablasController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    public void setConnection(JDBC connection) {
+        conection=connection;
+    }
+    public void setDataBaseSelected(String DataBaseSelected ){
+        dataBaseSelected=DataBaseSelected;
+    }
+    public static JDBC getConection(){
+        return conection;
+    }
 
     @FXML
-    private void click_modificar(ActionEvent event) {
+    private void click_modificar(ActionEvent event) throws IOException{
+        Parent MostrarParent = FXMLLoader.load(getClass().getResource("Modificar_tabla.fxml"));
+        Scene MostrarScene = new Scene(MostrarParent);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(MostrarScene);
+        window.show();
+
     }
 
     @FXML
@@ -62,7 +86,13 @@ public class TablasController implements Initializable {
     }
 
     @FXML
-    private void click_volver(ActionEvent event) {
+    private void click_volver(ActionEvent event) throws IOException {
+        Parent MostrarParent = FXMLLoader.load(getClass().getResource("Menu.fxml"));
+        Scene MostrarScene = new Scene(MostrarParent);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(MostrarScene);
+        window.show();
+
     }
 
     @FXML
@@ -70,7 +100,13 @@ public class TablasController implements Initializable {
     }
 
     @FXML
-    private void click_borrar_registro(ActionEvent event) {
+    private void click_borrar_registro(ActionEvent event)  throws IOException {
+        Parent MostrarParent = FXMLLoader.load(getClass().getResource("Eliminar_registro.fxml"));
+        Scene MostrarScene = new Scene(MostrarParent);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(MostrarScene);
+        window.show();
+
     }
 
     @FXML
