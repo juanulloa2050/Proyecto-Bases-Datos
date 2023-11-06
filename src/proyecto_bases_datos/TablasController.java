6ad13243ce888b3291885d96bfc7813675778098
juanulloa2050 <juanulloa2050@gmail.com>
2023-11-06 12:16:24 -0500

    Tablas controller

---
+++ src/proyecto_bases_datos/TablasController.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package proyecto_bases_datos;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TabPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author juanu
+ */
+public class TablasController implements Initializable {
+
+    @FXML
+    private TabPane TabPane_Tablas;
+    @FXML
+    private Button btn_buscar;
+    @FXML
+    private Button btn_modificar;
+    @FXML
+    private Button btn_crear;
+    @FXML
+    private Button btn_borrar;
+    @FXML
+    private Button btn_volver;
+    @FXML
+    private Button btn_modificar_registro;
+    @FXML
+    private Button btn_borar1;
+    @FXML
+    private Button btn_crear1;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void click_modificar(ActionEvent event) {
+    }
+
+    @FXML
+    private void click_crear(ActionEvent event) {
+    }
+
+    @FXML
+    private void click_borrar(ActionEvent event) {
+    }
+
+    @FXML
+    private void click_volver(ActionEvent event) {
+    }
+
+    @FXML
+    private void click_modificar_registro(ActionEvent event) {
+    }
+
+    @FXML
+    private void click_borrar_registro(ActionEvent event) {
+    }
+
+    @FXML
+    private void click_nuevo_dato(ActionEvent event) {
+    }
+    
+}
